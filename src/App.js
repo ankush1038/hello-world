0c3e2f3468ee0fd3d7e161d67d13976e2733628f
ankush1038 <ankush1038.be21@chitkarauniversity.edu.in>
2024-04-22 12:39:08 +0530

    New Components Added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import Welcome from './components/Welcome'
 import Message from './components/Message'
 import Counter from './components/Counter'
 import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
+       <ParentComponent /> 
 
-      <EventBind />
-        {/*<FunctionClick />
+
+      {/*<EventBind />
+        <FunctionClick />
 
       <Counter />
       
