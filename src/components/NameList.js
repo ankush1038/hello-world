5d955ad2619954f070c24342ce99e955c3b3807d
ankush1038 <ankush1038.be21@chitkarauniversity.edu.in>
2024-05-02 22:40:17 +0530

    Using key component

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,17 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Person from './Person'
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diana']
-    const nameList =  names.map(name => <h2>{name}</h2>)
-  
-    return (
-      <div>
-        {
-        // names.map(name => <h2>{name}</h2>)
-        nameList
-        }
-      </div>
-    )
+  const persons = [
+  {
+    id: 1,
+    name: 'Bruce',
+    age: 30,
+    skill: 'React'
+  },
+  {
+    id: 2,
+    name: 'Clark',
+    age: 100,
+    skill: 'Angular'
+  },
+  {
+    id: 3,
+    name: 'Diana',
+    age: 28,
+    skill: 'Vue'
   }
+  ]
+  const personList = persons.map(person => <Person key={persons.id} person={person} />)
+  return (
+    <div>
+      {personList}
+    </div>
+  )
+}
 
 export default NameList
+
