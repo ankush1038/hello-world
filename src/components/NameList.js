827da4624b3c3c7d5915edbacad30a065385425f
ankush1038 <ankush1038.be21@chitkarauniversity.edu.in>
2024-05-02 22:18:40 +0530

    List made

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 function NameList() {
     const names = ['Bruce', 'Clark', 'Diana']
+    const nameList =  names.map(name => <h2>{name}</h2>)
   
     return (
       <div>
-        <h2>{names[0]}</h2>
-        <h2>{names[1]}</h2>
-        <h2>{names[2]}.</h2>
+        {
+        // names.map(name => <h2>{name}</h2>)
+        nameList
+        }
       </div>
     )
   }
